24d1b942a0defa3a80859e7d3edcba822b32797f
jmales <jumales@gmail.com>
2020-06-09 18:30:44 +0200

    logging setup

--- src/test/java/com/jumales/library/repository/BookRepositoryTest.java
+++ src/test/java/com/jumales/library/repository/BookRepositoryTest.java
@@ -2,10 +2,14 @@ package com.jumales.library.repository;
 
 import com.jumales.library.TestAbstract;
 import com.jumales.library.entities.Book;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class IBookRepositoryTest  extends TestAbstract {
+public class BookRepositoryTest extends TestAbstract {
+
+    public static final Logger logger = LogManager.getLogger(BookRepositoryTest.class);
 
     public static final String BOOK_TITLE = "FIRST_BOOK";
     public static final String IBN = "1234";
@@ -16,7 +20,8 @@ public class IBookRepositoryTest  extends TestAbstract {
         b.setTitle(BOOK_TITLE);
         b.setIbn(IBN);
 
-        bookRepository.save(b);
+        Book savedBook = bookRepository.save(b);
+        logger.info("Book created: '{}'", savedBook);
 
         Book bookByIbn = bookRepository.findByIbn(IBN);
 
