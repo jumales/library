850b253765a136f4a0c24b76746d07cc148f4a45
jmales <jumales@gmail.com>
2020-06-10 14:41:17 +0200

    #3 Rest common things

---
+++ src/main/java/com/jumales/library/rest/api/IRestBase.java
@@ -0,0 +1,6 @@
+package com.jumales.library.rest.api;
+
+public interface IRestBase {
+    public static final String JSON_CONSUME = "application/json";
+    public static final String JSON_PRODUCE = "application/json";
+}
---
+++ src/main/java/com/jumales/library/rest/api/entity/StatusDTO.java
@@ -0,0 +1,46 @@
+package com.jumales.library.rest.api.entity;
+
+public class StatusDTO {
+
+    private int code;
+    private String shortMsg;
+    private String longMsg;
+
+    private StatusDTO(int code, String shortMsg, String longMsg){
+        this.code = code;
+        this.shortMsg = shortMsg;
+        this.longMsg = longMsg;
+    }
+
+    public static StatusDTO success(){
+        return new StatusDTO(200, "OK", null);
+    }
+
+    public static StatusDTO created(){
+        return new StatusDTO(201, "Created", null);
+    }
+
+    public static StatusDTO noContent(String message){
+        return new StatusDTO(204, "No content", message);
+    }
+
+    public static StatusDTO badRequest(String message){
+        return new StatusDTO(400, "Bad request", message);
+    }
+
+    public static StatusDTO unauthorized(String message){
+        return new StatusDTO(401, "Unauthorized", message);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getShortMsg() {
+        return shortMsg;
+    }
+
+    public String getLongMsg() {
+        return longMsg;
+    }
+}
