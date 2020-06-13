e811873fd27cff5274b97fba35262dfd7a2412a9
jmales <jumales@gmail.com>
2020-06-13 14:29:48 +0200

    #13

--- src/main/java/com/jumales/library/repository/IBookAuthorRepository.java
+++ src/main/java/com/jumales/library/repository/IBookAuthorRepository.java
@@ -3,5 +3,12 @@ package com.jumales.library.repository;
 import com.jumales.library.entities.BookAuthor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IBookAuthorRepository extends JpaRepository<BookAuthor, Long> {
+    List<BookAuthor> findByBookId(Long bookId);
+
+    List<BookAuthor> findByAuthorId(Long authorId);
+
+    BookAuthor findByBookIdAndAuthorId(Long bookId, Long authorId);
 }
